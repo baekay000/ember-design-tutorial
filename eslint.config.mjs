fa36748b620251895922072f1ab6321da8f586e2
kab08737 <kaylee.bae@kub.org>
2025-03-25 09:29:37 -0400

    ESlint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-import { eslint, merge } from "@nrg-ui/standards";
+import { eslint, merge } from '@nrg-ui/standards';
 
 export default await merge(
   eslint.rules.ignore([
-    "blueprints/*/files/",
-    "declarations/",
-    "coverage/",
-    "!.*",
-    ".*/",
-    ".node_modules.ember-try/",
+    '/blueprints/*/files/',
+    '/declarations/',
+    '/dist/',
+    '/coverage/',
+    '!.*',
+    '.*/',
+    '/.node_modules.ember-try/',
   ]),
   eslint.rules.base(),
-  eslint.rules.ember(),
+  // eslint.rules.ember(),
   eslint.rules.js(),
   eslint.rules.gjs(),
+  eslint.rules.scripts([
+    './.prettierrc.js',
+    './.stylelintrc.js',
+    './.template-lintrc.js',
+    './blueprints/*/index.js',
+    './config/**/*.js',
+    './ember-cli-build.js',
+    './eslint.config.mjs',
+    './lib/*/index.js',
+    './server/**/*.js',
+    './testem.js',
+  ]),
 );
